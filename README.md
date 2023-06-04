fe0d41e6eba095d26b89d9587c2efe6f0e7ceafc
Devin Norgarb <dnorgarb@gmail.com>
2023-06-04 11:47:49 +0200

    update project

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-wakadump-parser
+
+# Project Name
+
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+Short description or tagline for the project.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Features](#features)
+- [Contributing](#contributing)
+- [License](#license)
+- [Contact](#contact)
+
+## Installation
+
+1. Clone the repository:
+   ```shell
+   git clone https://github.com/DevinNorgarb/wakatime-parser.git
+   ```
+
+2. Navigate to the project directory:
+   ```shell
+   cd wakatime-parser
+   ```
+
+3. Install dependencies:
+   ```shell
+   composer install
+   ```
+
+## Usage
+
+1. Update the `input.json` file with your desired data.
+2. Run the PHP script with the following command:
+   ```shell
+   php your_script.php input.json
+   ```
+   Replace `parse.php` with the actual filename of your PHP script and `input.json` with the appropriate input file.
+
+3. View the generated output.
+
+## Features
+
+- Feature 1: Describe the feature and its benefits.
+- Feature 2: Describe the feature and its benefits.
+- ...
+
+## Contributing
+
+Contributions are welcome! Follow these steps to contribute:
+
+1. Fork the repository.
+2. Create a new branch: `git checkout -b feature/your-feature-name`
+3. Commit your changes: `git commit -m 'Add some feature'`
+4. Push to the branch: `git push origin feature/your-feature-name`
+5. Submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Contact
+
+For any inquiries or feedback, please contact:
+
+Your Name
+- Email: your-email@example.com
+- GitHub: [your-username](https://github.com/your-username)
+
+```
+
+Make sure to replace the placeholders with the actual information relevant to your project, such as project name, repository URL, installation instructions, usage guide, features, contributing guidelines, license information, and your contact details.
+
+Additionally, consider adding relevant badges or shields to the README, such as a license badge, build status badge, or any other badges relevant to your project. These badges can provide quick information and improve the visual appeal of your repository.
--- index.php
+++ index.php
@@ -1,67 +1,109 @@
 <?php
+    ini_set('memory_limit', '1024M');
 
-include 'vendor/autoload.php';
+    include 'vendor/autoload.php';
 
-use Carbon\Carbon;
-use Carbon\CarbonInterval;
+    use Carbon\Carbon;
+    use Carbon\CarbonInterval;
 
-$top_level_stuff = JsonMachine\JsonMachine::fromFile('wakatime-dnorgarbgmail.com-6757f838ed894ae78208a0883665098a.json');
+    if ($argc < 2) {
+        echo "Please provide the JSON file name as a command-line argument.\n";
+        exit(1);
+    }
+
+    $jsonFile = $argv[1];
+    $top_level_stuff = JsonMachine\JsonMachine::fromFile($jsonFile);
+
+    $total_seconds = 0;
+    $total_seconds_by_category = [];
+    $total_seconds_by_language = [];
+    $total_seconds_by_project = [];
+
+    foreach ($top_level_stuff as $k => $level_property) {
+        if ($k == 'days') {
+            foreach ($level_property as $num_day => $day) {
+                if (isset($day['categories'][0])) {
+                    $category_name = $day['categories'][0]['name'];
+                    $category_seconds = $day['categories'][0]['total_seconds'];
+                    $total_seconds += $category_seconds;
+
+                    if (!isset($total_seconds_by_category[$category_name])) {
+                        $total_seconds_by_category[$category_name] = 0;
+                    }
+                    $total_seconds_by_category[$category_name] += $category_seconds;
+                }
+
+                if (isset($day['languages'][0])) {
+
+                    foreach ($day['languages'] as $key => $langs) {
+                        $language_name = $langs['name'];
+                        $language_seconds = $langs['total_seconds'];
+                        $total_seconds += $language_seconds;
+
+                        if (!isset($total_seconds_by_language[$language_name])) {
+                            $total_seconds_by_language[$language_name] = 0;
+                        }
+                        $total_seconds_by_language[$language_name] += $language_seconds;
+                    }
+                }
 
 
-// [git@github.com:DevinNorgarb/wakadump-parser.git
-//     "categories" => array:1 [
-//       0 => array:9 [
-//         "decimal" => "6.73"
-//         "digital" => "6:44:44"
-//         "hours" => 6
-//         "minutes" => 44
-//         "name" => "Coding"
-//         "percent" => 100.0
-//         "seconds" => 44
-//         "text" => "6 hrs 44 mins"
-//         "total_seconds" => 24284.890617132
-//       ]
-//     ]
-$total_seconds = 0;
+                if (isset($day['projects'][0])) {
+                    foreach ($day['projects'] as $key => $project) {
+                        $project_name = $project['name'];
+//                    dump($project_name . ': ' . $project['grand_total']['total_seconds']);
+                        $project_seconds = $project['grand_total']['total_seconds'];
+                        $total_seconds += $project_seconds;
 
-$groupByDate = [];
-
-foreach ($top_level_stuff as $k => $level_property) {
-    if ($k == 'days') {
-
-        // Each day.
-        foreach ($level_property as $num_day => $day) {
-
-            $groupByDate[$day['date']] = [];
-            //Time values
-            if (isset($day['categories'][0])) {
-                $total_seconds += $day['categories'][0]['total_seconds'];
-                $groupByDate[$day['date']]['seconds'] = $day['categories'][0]['total_seconds'];
-            }
-
-            if (isset($day['languages'][0])) {
-                foreach ($day['languages'] as $key => $langs) {
-                    $total_seconds = $langs;;
-                    $groupByDate[$day['date']]['languages'][] =  $langs['name'] . ': ' . $langs['percent'];
+                        if (!isset($total_seconds_by_project[$project_name])) {
+                            $total_seconds_by_project[$project_name] = 0;
+                        }
+                        $total_seconds_by_project[$project_name] += $project_seconds;
+                    }
                 }
             }
         }
     }
-}
 
-$time_behind_pc = CarbonInterval::seconds($total_seconds)->cascade()->forHumans();
+    $time_behind_pc = CarbonInterval::seconds($total_seconds)->cascade()->forHumans();
 
+    $dt = Carbon::now();
+    $days = $dt->diffInDays($dt->copy()->addSeconds($total_seconds));
+    $hours = $dt->diffInHours($dt->copy()->addSeconds($total_seconds));
+    $minutes = $dt->diffInMinutes($dt->copy()->addSeconds($total_seconds));
 
-$dt = Carbon::now();
-$days = $dt->diffInDays($dt->copy()->addSeconds($total_seconds));
-$hours = $dt->diffInHours($dt->copy()->addSeconds($total_seconds));
-$minutes = $dt->diffInMinutes($dt->copy()->addSeconds($total_seconds));
+    echo 'Total time tracked: ' . "\n";
+    echo '-------------------' . "\n";
+    echo 'Total in days: ' . $days . "\n";
+    echo 'Total in hours: ' . $hours . "\n";
+    echo 'Total in minutes: ' . $minutes . "\n";
+    echo 'Total time behind PC: ' . $time_behind_pc . "\n";
 
+    echo '-------------------' . "\n";
+    echo 'Time tracked by category:' . "\n";
+    $total_number_of_seconds_by_category_for_humans = [];
+    foreach ($total_seconds_by_category as $category_name => $category_seconds) {
+        $category_time = CarbonInterval::seconds($category_seconds)->cascade()->forHumans();
+        $total_number_of_seconds_by_category_for_humans[$category_name] = $category_time;
 
-dump(
-    "Total in days " . $days . PHP_EOL .
-        "Total in hours " . $hours . PHP_EOL .
-        "Total in minutes " . $minutes . PHP_EOL
-);
-dump("-----------------");
-dump($time_behind_pc);
+    }
+
+    dump( '-------------------' . "\n");
+    dump( 'Time tracked by language:' . "\n");
+    $total_number_of_seconds_by_language_for_humans = [];
+    foreach ($total_seconds_by_language as $language_name => $language_seconds) {
+        $language_time = CarbonInterval::seconds($language_seconds)->cascade()->forHumans();
+        $total_number_of_seconds_by_language_for_humans[$language_name] = $language_time;
+
+    }
+
+    dump( '-------------------' . "\n");
+    dump( 'Time tracked by project:' . "\n");
+    $total_number_of_seconds_by_project_for_humans = [];
+    foreach ($total_seconds_by_project as $project_name => $project_seconds) {
+        $project_time = CarbonInterval::seconds($project_seconds)->cascade()->forHumans();
+        $total_number_of_seconds_by_project_for_humans[$project_name] = $project_time;
+
+    }
+    dump($total_number_of_seconds_by_category_for_humans, $total_number_of_seconds_by_language_for_humans, $total_number_of_seconds_by_project_for_humans);
+?>
